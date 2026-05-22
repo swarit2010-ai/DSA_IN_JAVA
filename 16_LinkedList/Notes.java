b6d9b7806a974dfc9adcb54ca88b57001bb5ac9f
Swarit <swaritsharma2010@gmail.com>
2026-05-22 17:45:30 +0530

    Continued linked list.Very nice progress

--- 16_LinkedList/Notes.java
+++ 16_LinkedList/Notes.java
@@ -23,3 +23,65 @@ Problems in linked list
 We cannot access the elements in the middle so easily 
 it needs traversal of the whole linked list
 */
+
+
+public class Notes{
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(20); 
+        Node c = new Node(30); 
+        Node d = new Node(40); 
+        Node e = new Node(50);
+        Node f = new Node(60);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        // displaying line by line
+        display(a);
+        System.out.println();
+        // displaying line by line and then reverse
+        // using recursion
+        display_recur(a);
+        // Getting an element
+        System.out.println();
+        System.out.println(get(a,2,0));
+        System.out.println(get(a,2));
+    }
+    public static void display(Node head){
+        // in while loop
+        // Node temp = head; // temp gains head's reference
+        // while(temp != null){
+        //     System.out.print(temp.val + " ");
+        //     temp = temp.next;
+        // }
+        // in for loop
+        for(Node temp = head;temp != null;temp = temp.next){
+            System.out.print(temp.val + " ");
+        }
+    }
+    public static void display_recur(Node head){
+        if(head == null) {
+            System.out.println();
+            return;
+        }
+        System.out.print(head.val + " ");
+        display_recur(head.next);
+        System.out.print(head.val + " ");
+    }
+
+    private static int get(Node a, int idx , int i) {
+        if(i == idx){
+            return a.val;
+        }
+        return(get(a.next, idx, i+1));
+    }
+    private static int get(Node head, int idx) {
+        Node temp = head;
+        for(int i = 0;i < idx;i++){
+            temp = temp.next;
+        }
+        return temp.val;
+    }
+}
---
+++ 16_LinkedList/Pro_01.java
@@ -0,0 +1,31 @@
+import java.util.*;
+/* You are given the head of a singly linked list. Return an array containing the values of the nodes.
+
+Examples:
+
+Input:
+    
+Output: [1, 2, 3, 4, 5]
+Explanation: The linked list contains 5 elements [1, 2, 3, 4, 5]. The elements are printed in a single line.
+Input:
+    
+Output: [10, 20, 30, 40, 50, 60]
+Explanation: The linked list contains 5 elements [10, 20, 30, 40, 50, 60]. The elements are printed in a single line.
+Constraints :
+1 ≤ numbers of nodes ≤ 105
+1 ≤ node values ≤ 106
+*/
+public class Pro_01{
+    public static void main(String[] args) {
+    }
+    public ArrayList<Integer> printList(Node head) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        Node temp = head;
+        while(temp != null) {
+            ans.add(temp.val);
+            temp = temp.next;
+        }
+        return ans;
+    }
+}
