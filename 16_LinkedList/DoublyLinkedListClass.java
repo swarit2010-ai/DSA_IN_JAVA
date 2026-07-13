6db9cd67a77e8f68addc61eb5941cfb0e2b98b13
Swarit <swaritsharma2010@gmail.com>
2026-07-13 17:36:37 +0530

    Continued linked list

---
+++ 16_LinkedList/DoublyLinkedListClass.java
@@ -0,0 +1,104 @@
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode prev;
+    ListNode(int val){
+        this.val = val;
+    }
+}
+class DLL{
+    ListNode head;
+    ListNode tail;
+    int size;
+    void display(){
+        ListNode temp = head;
+        while(temp != null){
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+    }
+    void display_reverse(){
+        ListNode temp = tail;
+        while(temp != null){
+            System.out.print(temp.val + " ");
+            temp = temp.prev;
+        }
+    }
+    void insertAtHead(int val){
+        ListNode temp = new ListNode(val);
+        if(head == null) head = tail = temp;
+        else{
+            temp.next = head;
+            head.prev = temp;
+            head = temp;
+        }
+        size++;
+    }
+    void insertAtTail(int val){
+        ListNode temp = new ListNode(val);
+        if(head == null) head = tail = temp;
+        else{
+            tail.next = temp;
+            temp.prev = tail;
+            tail = temp;
+        }
+        size++;
+    }
+    void deleteAtHead(){
+        if(size == 0) return;
+        if(size == 1) head = tail = null;
+        else{
+            head = head.next;
+            head.prev = null;
+        }
+        size--;
+    }
+    void deleteAtTail(){
+        if(size == 0) return;
+        if(size == 1) head = tail = null;
+        else{
+            tail = tail.prev;
+            tail.next = null;
+        }
+        size--;
+    }
+    void insert(int idx,int val){
+        if(idx < 0 || idx > size) return;
+        if(idx == size){
+            insertAtTail(val);
+            return;
+        }
+        ListNode temp = head;
+        ListNode new_node = new ListNode(val);
+        if(size == 0) head = tail = new_node;
+        else{
+            for(int i = 1;i <= idx-1;i++){
+                temp = temp.next;
+            }
+            new_node.prev = temp;
+            new_node.next = temp.next;
+            temp.next = new_node;
+            new_node.next.prev = new_node;
+        }
+        size++;
+    }
+}
+public class DoublyLinkedListClass {
+    public static void main(String[] args) {
+        DLL list = new DLL();
+        list.insertAtHead(10);
+        list.insertAtTail(50);
+        // list.display();
+        list.deleteAtHead();
+        // list.display();
+        list.deleteAtTail();
+        list.insertAtHead(50);
+        list.insertAtHead(10);
+        list.display();
+        System.out.println();
+        list.display_reverse();
+        System.out.println();
+        list.insert(2, 10);
+        list.display();
+    }
+}
---
+++ 16_LinkedList/Pro_49.java
@@ -0,0 +1,43 @@
+/* Reverse a Doubly Linked List
+Difficulty: EasyAccuracy: 70.38%Submissions: 222K+Points: 2Average Time: 15m
+Given the head of a doubly linked list. You have to reverse the doubly linked list and return its head.
+
+Note: The driver code will print the returned list in both forward and backward directions.
+
+Examples:
+
+Input:
+   
+Output: 
+5 4 3
+3 4 5
+Explanation: After reversing the given doubly linked list the new list will be 5 <-> 4 <-> 3.
+   
+Input: 
+   
+Output: 
+196 59 122 75
+75 122 59 196
+Explanation: After reversing the given doubly linked list the new list will be 196 <-> 59 <-> 122 <-> 75.
+   
+Constraints:
+1 ≤ number of nodes ≤ 106
+0 ≤ node->data ≤ 104*/
+public class Pro_49{
+    public static void main(String[] args) {
+    }
+    public ListNode reverse(ListNode head) {
+        // code here
+        ListNode p = null;
+        ListNode c = head;
+        ListNode f = head;
+        while(c != null){
+            f = c.next;
+            c.next = p;
+            c.prev = f;
+            p = c;
+            c = f;
+        }
+        return p;
+    }
+}
