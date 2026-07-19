432f89a6ba90b7d23f98aa341813f79863ec428b
Swarit <swaritsharma2010@gmail.com>
2026-07-19 23:42:35 +0530

    Continued stacks

--- 17_Stacks/Pro_02.java
+++ 17_Stacks/Pro_02.java
@@ -17,7 +17,41 @@ Explanation: After reversing, the elements of stack are in opposite order.
 Constraints:
 1 ≤ st.size() ≤ 100
 0 ≤ stack element ≤ 100 */
+
+import java.util.Stack;
+
 public class Pro_02{
     public static void main(String[] args) {
+    }
+    public static void reverseStack(Stack<Integer> st) {
+        // code here
+        // Two extra stack method:
+        // Stack<Integer> st2 = new Stack<>();
+        // while(st.size() > 0) st2.push(st.pop());
+        // Stack<Integer> st3 = new Stack<>();
+        // while(st2.size() > 0) st3.push(st2.pop());
+        // while(st3.size() > 0) st.push(st3.pop());
+        // Array method
+        // int[] arr = new int[st.size()];
+        // int idx = 0;
+        // while(st.size() > 0){
+        //     arr[idx] = st.pop();
+        //     idx++;
+        // }
+        // for(int i = 0;i < arr.length;i++) st.push(arr[i]);
+        // Recursive call stack method
+        if(st.size() == 0) return;
+        int top = st.pop();
+        reverseStack(st);
+        insertAtBottom(st,top);
+    }
+    public static void insertAtBottom(Stack<Integer> st, int x) {
+        if(st.isEmpty()){
+            st.push(x);
+            return;
+        }
+        int top = st.pop();
+        insertAtBottom(st,x);
+        st.push(top);
     }
 }
