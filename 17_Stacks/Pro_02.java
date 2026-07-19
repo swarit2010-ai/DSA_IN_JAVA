e0dec309aab1ed4200dddb84406bbf9e45b88ad4
Swarit <swaritsharma2010@gmail.com>
2026-07-19 15:03:37 +0530

    Current development progress -> Completed linked list,started stacks and will do queues in this month July itself.Will also progress in the gfg paid course of DSA from August first week along with starting binary trees and other data structures.Planning to complete DSA till December and do a lot of practice till March as thats the last month of class 10 and paradise

---
+++ 17_Stacks/Pro_02.java
@@ -0,0 +1,23 @@
+/*Reverse a Stack
+Difficulty: MediumAccuracy: 80.5%Submissions: 136K+Points: 4Average Time: 20m
+You are given a stack st[]. You have to reverse the stack.
+
+Note: The input array represents the stack from bottom to top (last element is the top). The output is displayed by printing elements from top to bottom after reversal.
+
+Examples:
+
+Input: st[] = [1, 2, 3, 4]
+Output: [1, 2, 3, 4]
+Explanation: After reversing, the elements of stack are in opposite order.
+
+Input: st[] = [3, 2, 1]
+Output: [3, 2, 1]
+Explanation: After reversing, the elements of stack are in opposite order.
+
+Constraints:
+1 ≤ st.size() ≤ 100
+0 ≤ stack element ≤ 100 */
+public class Pro_02{
+    public static void main(String[] args) {
+    }
+}
